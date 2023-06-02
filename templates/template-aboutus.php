25aaaacc5eb22ae511ca272b8236f9acc4b71b14
Rafael Poli <rafaelpoli@gmail.com>
2023-06-02 12:25:38 -0300

    Assets

---
+++ templates/template-aboutus.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: Sobre Nós
+*/
+
+get_header();
+?>
+
+<section class="sobre-nos">
+    <h1><?= the_title() ?></h1>
+    <h2>Norem ipsum dolor sit amet,<br> consectetur adipiscing elit.</h2>
+    <p><?= the_content() ?></p>
+</section>
+
+
+<?php
+   get_footer();
+?>
+
+<?php
+get_footer();
+?>
